736e41343b276bb149a279425cb8951589a83753
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-07-27 13:05:38 +0200

    use _jupyter_config

--- viasp_jupyter/viasp_jupyter.py
+++ viasp_jupyter/viasp_jupyter.py
@@ -4,11 +4,14 @@ import viasp_dash
 from jupyter_dash import JupyterDash
 from viasp import Control
 
+from jupyter_dash.comms import _jupyter_config
 
+_viasp_backend_url = _jupyter_config['base_subpath'].rstrip('/') + '/proxy/5050/'
+print(_viasp_backend_url)
 def load(argv):
     options = ["0"]
 
-    ctl = Control(options, viasp_backend_url="http://localhost:5050")
+    ctl = Control(options, viasp_backend_url=_viasp_backend_url)
     for path in argv:
         ctl.load(path)
     if not argv:
@@ -27,7 +30,7 @@ app = JupyterDash(__name__)
 
 app.layout = viasp_dash.ViaspDash(
     id="myID",
-    backendURL="http://localhost:5050"
+    backendURL=_viasp_backend_url
 )
 
 subprocess.Popen(["viasp"],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
