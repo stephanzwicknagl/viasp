f08a7708997634f58c578d6bb9fc11733bb599b9
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-07-28 17:01:58 +0200

    change viasp call

--- viasp_jupyter/viasp_jupyter.py
+++ viasp_jupyter/viasp_jupyter.py
@@ -51,6 +51,6 @@ app.layout = viasp_dash.ViaspDash(
     backendURL=_viasp_backend_url
 )
 
-subprocess.Popen(["viasp", "--host", _default_server_url, "-p",
-                 _default_requests_pathname_prefix])  # , stdout=subprocess.DEVNULL,
+subprocess.Popen(["viasp", "--host", _viasp_backend_url, "-p",
+                 '443'])  # , stdout=subprocess.DEVNULL,
                 #  stderr=subprocess.DEVNULL)
