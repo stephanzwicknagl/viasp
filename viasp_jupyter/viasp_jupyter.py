319f6c4d19de1705bbdd8828fe7fef48184d4b98
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-08-10 15:14:12 +0200

    localhost on binder

--- viasp_jupyter/viasp_jupyter.py
+++ viasp_jupyter/viasp_jupyter.py
@@ -22,14 +22,12 @@ if ('server_url' in _jupyter_config):
 # else:
     # _viasp_backend_url = "http://localhost:5050"
 
-_viasp_backend_url = _default_server_url+_default_requests_pathname_prefix
-print(_viasp_backend_url)
 
 
 def load(argv):
     options = ["0"]
 
-    ctl = Control(options, viasp_backend_url=_viasp_backend_url)
+    ctl = Control(options, viasp_backend_url="http://localhost:5050")
     for path in argv:
         ctl.load(path)
     if not argv:
@@ -48,9 +46,8 @@ app = JupyterDash(__name__)
 
 app.layout = viasp_dash.ViaspDash(
     id="myID",
-    backendURL=_viasp_backend_url
+    backendURL="http://localhost:5050"
 )
 
-subprocess.Popen(["viasp", "--host", _viasp_backend_url, "-p",
-                 '5050'])  # , stdout=subprocess.DEVNULL,
+subprocess.Popen(["viasp"])  # , stdout=subprocess.DEVNULL,
                 #  stderr=subprocess.DEVNULL)
