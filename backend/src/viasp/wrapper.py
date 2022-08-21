c1ec48b709559263931f28e12f1ac862eb3e42d9
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-08-21 14:19:00 +0200

    new class AppControl knows that innercontrol already exists

--- backend/src/viasp/wrapper.py
+++ backend/src/viasp/wrapper.py
@@ -55,22 +55,8 @@ class Control(InnerControl):
             del kwargs["_viasp_client"]
         if "viasp_backend_url" in kwargs:
             del kwargs["viasp_backend_url"]
-        if "control" in kwargs:
-            self._free = kwargs['control']._free
-            self._mem = kwargs['control']._mem
-            # c_handle = kwargs['control'].c_handle
-            # c_cb = kwargs['control'].c_cb
-            # c_args = kwargs['control'].c_args
-            self._rep = kwargs['control']._rep
-            self._handler = kwargs['control']._handler
-            self._statistics = kwargs['control']._statistics
-            self._statistics_call = kwargs['control']._statistics_call
-            self._error = kwargs['control']._error
-            del kwargs["control"]
-            self.viasp.register_function_call("__init__", signature(super().__init__), args, kwargs)
-        else:
-            self.viasp.register_function_call("__init__", signature(super().__init__), args, kwargs)
-            super().__init__(*args, **kwargs)
+        self.viasp.register_function_call("__init__", signature(super().__init__), args, kwargs)
+        super().__init__(*args, **kwargs)
 
     def load(self, path: str) -> None:
         if path == "-":
@@ -86,6 +72,51 @@ class Control(InnerControl):
 
     def __getattribute__(self, name):
         attr = InnerControl.__getattribute__(self, name)
+        if is_non_cython_function_call(attr) and name != "load":
+            def wrapper_func(*args, **kwargs):
+                self.viasp.register_function_call(attr.__name__, signature(attr), args, kwargs)
+                result = attr(*args, **kwargs)
+                return result
+
+            return wrapper_func
+        else:
+            return attr
+
+
+class AppControl(InnerControl):
+
+    def __init__(self, *args, **kwargs):
+        self.viasp = ShowConnector(**kwargs)
+        print(f"Connecting to backend at {kwargs['viasp_backend_url']}")
+        if "_viasp_client" in kwargs:
+            del kwargs["_viasp_client"]
+        if "viasp_backend_url" in kwargs:
+            del kwargs["viasp_backend_url"]
+        if "control" in kwargs:
+            self.inctl = kwargs['control']
+            del kwargs["control"]
+            self.viasp.register_function_call("__init__", signature(self.inctl.__init__), args, kwargs)
+        else:
+            self.viasp.register_function_call("__init__", signature(super().__init__), args, kwargs)
+            super().__init__(*args, **kwargs)
+
+    def load(self, path: str) -> None:
+        if path == "-":
+            path = STDIN_TMP_STORAGE_PATH
+            tmp = sys.stdin.readlines()
+            with open(path, "w", encoding="utf-8") as f:
+                f.writelines(tmp)
+        else:
+            shutil.copy(path, STDIN_TMP_STORAGE_PATH)
+            path = STDIN_TMP_STORAGE_PATH
+        self.viasp.register_function_call("load", signature(self.inctl.load), [], kwargs={"path": path})
+        self.inctl.load(path=str(path))
+        
+    def __getattribute__(self, name):
+        if name == "ground" or name == "solve": 
+            attr = self.inctl.__getattribute__(name)
+        else:
+            attr = InnerControl.__getattribute__(self, name)
         if is_non_cython_function_call(attr) and name != "load":
             def wrapper_func(*args, **kwargs):
                 self.viasp.register_function_call(attr.__name__, signature(attr), args, kwargs)
