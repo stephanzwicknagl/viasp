ddfde8fce4493b788c0583f3098549ef5747d086
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-09-10 17:26:37 +0200

    add control2

--- backend/src/viasp/wrapper.py
+++ backend/src/viasp/wrapper.py
@@ -98,6 +98,59 @@ class Control(InnerControl):
             return attr
 
 
+class Control2:
+    
+    
+    def __init__(self, *args, **kwargs):
+        if "files" in kwargs:
+            self.arguments = ([opt for opt in sys.argv[1:] if opt not in kwargs["files"]])
+            kwargs["arguments"] = self.arguments
+            del kwargs["files"]
+
+        if "control"  in kwargs:
+            self.passed_control = kwargs['control']
+            del kwargs['control']
+        else:
+            self.passed_control = Control(kwargs["arguments"])
+
+        self.viasp = ShowConnector(**kwargs)
+        print(f"Connecting to backend at {kwargs['viasp_backend_url']}")
+
+        if "_viasp_client" in kwargs:
+            del kwargs["_viasp_client"]
+        if "viasp_backend_url" in kwargs:
+            del kwargs["viasp_backend_url"]
+        
+        self.viasp.register_function_call("__init__", signature(self.passed_control.__init__), args, kwargs)
+
+    def load(self, path: str) -> None:
+        if path == "-":
+            path = STDIN_TMP_STORAGE_PATH
+            tmp = sys.stdin.readlines()
+            with open(path, "w", encoding="utf-8") as f:
+                f.writelines(tmp)
+        else:
+            shutil.copy(path, STDIN_TMP_STORAGE_PATH)
+            path = STDIN_TMP_STORAGE_PATH
+        self.viasp.register_function_call("load", signature(self.passed_control.load), [], kwargs={"path": path}) #? or self.passed_control.load
+        self.passed_control.load(path=str(path))
+
+
+    def __getattribute__(self, name):
+        try:
+            attr = object.__getattribute__(self, name)
+        except AttributeError:
+            attr = self.passed_control.__getattribute__(name) 
+        if is_non_cython_function_call(attr) and name != "load":
+            def wrapper_func(*args, **kwargs):
+                self.viasp.register_function_call(attr.__name__, signature(attr), args, kwargs)
+                result = attr(*args, **kwargs)
+                return result
+
+            return wrapper_func
+        else:
+            return attr
+
 class EnhancedJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if is_dataclass(o):
