9091d7e8ccf6bb2f2d01fa77d45692f28c2912cb
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-09-13 12:43:53 +0200

    improve jupyter mode

--- backend/src/viasp/startup.py
+++ backend/src/viasp/startup.py
@@ -14,6 +14,7 @@ from subprocess import Popen
 import atexit
 import viasp_dash
 from viasp import clingoApiClient
+from time import time
 from viasp.shared.defaults import (
                                   DEFAULT_BACKEND_HOST,
                                   DEFAULT_BACKEND_PORT,
@@ -25,12 +26,12 @@ def run(mode="dash", host=DEFAULT_BACKEND_HOST, port=DEFAULT_BACKEND_PORT):
     backend_url = f"{DEFAULT_BACKEND_PROTOCOL}://{host}:{port}"
     command = ["viasp", "--host", host, "--port", str(port)]
 
-    if mode == "dash":
-        from dash import Dash
-        app = Dash(__name__)
-    elif mode == "jupyter":
+    if mode == "jupyter":
         from jupyter_dash import JupyterDash
         app = JupyterDash(__name__)
+    else:
+        from dash import Dash
+        app = Dash(__name__)
     app.layout = viasp_dash.ViaspDash(
         id="myID",
         backendURL=backend_url
@@ -38,10 +39,14 @@ def run(mode="dash", host=DEFAULT_BACKEND_HOST, port=DEFAULT_BACKEND_PORT):
 
     log = open('viasp.log', 'a', encoding="utf-8")
     viasp_backend = Popen(command, stdout=log, stderr=log)
+
     # make sure the backend is up, before continuing with other modules
+    t = time()
     while True:
         if clingoApiClient.backend_is_running(backend_url):
             break
+        if time() - t > 10:
+            raise Exception("Backend did not start in time.")
 
     def terminate_process(process):
         """ kill the backend on keyboard interruptions"""
