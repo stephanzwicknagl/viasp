5d17eb21d3ff6eabb2535eae44e6c64615836c59
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-10-12 21:26:37 +0200

    update AST Types for clingo 5.6

--- backend/src/viasp/asp/ast_types.py
+++ backend/src/viasp/asp/ast_types.py
@@ -3,19 +3,17 @@ from clingo.ast import ASTType
 SUPPORTED_TYPES = {
     ASTType.Comparison, ASTType.Aggregate, ASTType.Rule, ASTType.Program, ASTType.ShowSignature, ASTType.Definition,
     ASTType.Literal, ASTType.HeadAggregate, ASTType.HeadAggregateElement, ASTType.BodyAggregate, ASTType.Aggregate,
-    ASTType.ConditionalLiteral, ASTType.AggregateGuard, ASTType.Comparison, ASTType.SymbolicAtom, ASTType.Function,
+    ASTType.ConditionalLiteral, ASTType.Guard, ASTType.Comparison, ASTType.SymbolicAtom, ASTType.Function,
     ASTType.BodyAggregateElement, ASTType.BooleanConstant, ASTType.SymbolicAtom, ASTType.Variable, ASTType.SymbolicTerm,
     ASTType.Interval, ASTType.UnaryOperation, ASTType.BinaryOperation
 }
 
 UNSUPPORTED_TYPES = {
-    ASTType.CspProduct, ASTType.CspSum, ASTType.CspGuard, ASTType.CspLiteral,
-    ASTType.Disjunction, ASTType.DisjointElement, ASTType.Disjoint,
+    ASTType.Disjunction,
     ASTType.TheorySequence, ASTType.TheoryFunction, ASTType.TheoryUnparsedTermElement, ASTType.TheoryUnparsedTerm,
     ASTType.TheoryGuard, ASTType.TheoryAtomElement, ASTType.TheoryAtom, ASTType.TheoryOperatorDefinition,
     ASTType.TheoryTermDefinition, ASTType.TheoryGuardDefinition, ASTType.TheoryAtomDefinition,
 }
-
 
 def make_unknown_AST_enum_types():
     known = UNSUPPORTED_TYPES.union(SUPPORTED_TYPES)
