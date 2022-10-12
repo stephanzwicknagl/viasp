5d7dcd5a5d9a5d478c1fb8f4326faaefaac65764
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2022-10-12 15:53:19 +0200

    complete relaxer

--- backend/src/viasp/asp/relax.py
+++ backend/src/viasp/asp/relax.py
@@ -1,28 +1,18 @@
-from typing import List
-from clingo.ast import AST, Symbol, Id, Rule, SymbolicTerm, Function, Literal, SymbolicAtom
+from typing import List, Set
+from clingo.ast import (AST, SymbolicTerm, Function, Literal,
+                    SymbolicAtom, ASTSequence, parse_string, Sign)
 
-import clingo
-import clingo.ast
-from clingo.ast import parse_string
+from clingo import Function as ClingoFunction
 from .utils import is_constraint
 
 class Relaxer:
     def visit_children(self, x, *args, **kwargs):
-        with open("transform.log", "a") as f:
-            f.write(f"visit_children of {x}\n")
-
         for key in x.child_keys:
-            with open("transform.log", "a") as f:
-                f.write(f"key {key}\n")
             setattr(x, key, self.visit(getattr(x, key), *args, **kwargs))
         return x
 
     def visit(self, x, *args, **kwargs):
-        with open("transform.log", "a") as f:
-            f.write(f"ast {x}\n")
         if isinstance(x, AST):
-            with open("transform.log", "a") as f:
-                f.write(f"ast type: {x.ast_type}\n")
             attr = 'visit_' + str(x.ast_type).replace('ASTType.', '')
             if hasattr(self, attr):
                 return getattr(self, attr)(x, *args, **kwargs)
@@ -30,28 +20,53 @@ class Relaxer:
                 return self.visit_children(x, *args, **kwargs)
         elif isinstance(x, list):
             return [self.visit(y, *args, **kwargs) for y in x]
+        elif isinstance(x, ASTSequence):
+            return [self.visit(y, *args, **kwargs) for y in x]
         elif x is None:
             return x
         else:
             raise TypeError("unexpected type")
 
-class TermRelaxer(Relaxer):
-    def __init__(self):
-        pass
 
-    def visit_Function(self, term):
-        term.arguments.append(Symbol(term.location))
+class TermRelaxer(Relaxer):
+    """
+        Transformer for visiting a term and collecting variables for the head.
+        Only Variables from positive literals, comparisons and guards are collected.
+    """
+    def __init__(self):
+        self.collector: Set[AST] = set()
+
+    def visit_Literal(self, literal):
+        """
+            Visit a literal. If it is positive or double negation, visit its children.
+        """
+        if literal.sign != Sign.Negation:
+            _ = self.visit_children(literal)
+        return literal
+
+    def visit_Variable(self, term):
+        """
+            Visit a variable. Add it to the collector.
+        """
+        self.collector.add(term)
         return term
 
-    # def visit_Symbol(self, term):
-    #     # this function is not necessary if gringo's parser is used
-    #     # but this case could occur in a valid AST
-    #     fun = term.symbol
-    #     assert(fun.type == clingo.SymbolType.Function)
-    #     term.symbol = clingo.Function(fun.name, fun.arguments + [self], fun.positive)
-    #     return term
+    def visit_BodyAggregateElement(self, term):
+        """
+            Visit a body aggregate element. Do not visit its children.
+        """
+        return term
+
+    def empty_collector(self):
+        self.collector = set()
+
 
 class ProgramRelaxer(Relaxer):
+    """
+        transformer for ASP programs. Relaxes constraints by adding a new rule head.
+        Only rules are modified, but all statement types are visited to return a complete program.
+    """
+
     def __init__(self, head_name="unsat", get_variables=True):
         self.head_name: str = head_name
         self.get_variables: bool = get_variables
@@ -59,73 +74,59 @@ class ProgramRelaxer(Relaxer):
         self.term_relaxer = TermRelaxer()
         self.constraint_counter: int = 1
 
-    # def visit_SymbolicAtom(self, atom):
-    #     atom.term = self.term_transformer.visit(atom.term)
-    #     return atom
-
     def visit_Rule(self, rule):
+        """
+            Visit a rule. If it is a constraint, relax it.
+        """
         if is_constraint(rule):
-            with open("transform.log", "a") as f:
-                f.write(f"rule is a constraint: {rule} \n")
-            arguments = List[AST]
-            if self.get_variables:
-                with open("transform.log", "a") as f:
-                    f.write(f"rule body: {rule.body}\n")
-                # get arguments of body:
-                for term in rule.body:
-                    with open("transform.log", "a") as f:
-                        f.write(f"term: {term}\n")
-                    arguments.append(self.term_relaxer.visit(term)) #Q which terms are relevant?
-                # create new rule head:
-            rule.head = self._make_head_literal(rule, arguments)
+            args = self._get_variables(rule)
+            # create new rule head:
+            rule.head = self._make_head_literal(rule, args)
         self._register_relaxed_program(rule)
+        return rule
 
     def visit_Program(self, prg):
-        # prg.parameters.append(Id(prg.location, self.parameter.name))
+        """
+            Visit a program.
+        """
+        self._register_relaxed_program(prg)
         return prg
 
-    # def visit_ShowSignature(self, sig):
-    #     sig.arity += 1
-    #     return sig
+    def relax_constraints(self, program: str) -> list[str]:
+        """
+            Relax constraints in a program and add minimization statement.
 
-    # def visit_ProjectSignature(self, sig):
-    #     sig.arity += 1
-    #     return sig
-    
-    def relax_constraints(self, program: str) -> str:
-        with open("transform.log", "a") as f:#
-            f.write(f"program: {program}\n")
+            E.g.: :- a(X). -> unsat(r1,(X,)):- a(X).:~unsat(R,T).[1,R,T]
+        """
         parse_string(program, lambda statement: self.visit(statement))
-        stringified = "".join(map(str, self.relaxed_program))
+        stringified = list(map(str, self.relaxed_program))
 
-        if self.get_variables:
-            stringified += ":~ unsat(R,T).[1,R,T]"
-        else:
-            stringified += ":~ unsat(R).[1,R]"
+        stringified.append(f":~ {self.head_name}(R,T).[1,R,T]" if
+                    self.get_variables else f":~ {self.head_name}(R).[1,R]")
         return stringified
-    
+
     def _register_relaxed_program(self, rule: AST):
         self.relaxed_program.append(rule)
 
-    def _make_head_literal(self, rule: Rule, inner_args: List[AST]) -> Literal:
+    def _make_head_literal(self, rule: AST, inner_args: Set[AST]) -> AST:
         location = rule.head.location
-        args = [SymbolicTerm(location, clingo.Function(f'r{self._constraint_number()}', [], True))]
-        if inner_args != []:
-            args = args + inner_args
+        args = [SymbolicTerm(location, ClingoFunction(f'r{self._constraint_number()}', [], True))]
+        if inner_args:
+            args.append(Function(location, '', list(inner_args),0))
         atom = SymbolicAtom(Function(location, self.head_name, args, 0))
-
         return Literal(location, 0, atom)
-    
+
     def _constraint_number(self) -> int:
         i = self.constraint_counter
         self.constraint_counter += 1
         return i
 
-def main(prg):
-    with prg.builder() as b:
-        t = ProgramRelaxer(clingo.Function("k"))
-        clingo.parse_program(
-            open("example.lp").read(),
-            lambda stm: b.add(t.visit(stm)))
-    prg.ground([("base", [clingo.Number(i)]) for i in range(3)])
-    prg.solve()
+    def _get_variables(self, rule: AST):
+        if not self.get_variables:
+            return set()
+        # reset the arg collector
+        self.term_relaxer.empty_collector()
+        # get arguments of body:
+        for term in rule.body:
+            _ = self.term_relaxer.visit(term)
+        return self.term_relaxer.collector
