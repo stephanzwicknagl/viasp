739cdc1390c4c6c7b29dcf1188951cdc80e46c2c
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2023-02-22 10:26:25 -1000

    add bauchbinde, style

--- frontend/src/lib/components/Node.react.js
+++ frontend/src/lib/components/Node.react.js
@@ -196,7 +196,9 @@ function OverflowButton(props) {
 
     return <div style={{ "backgroundColor": colorPalette.ten.dark, "color": colorPalette.sixty.dark }}
                 className={"bauchbinde"} onClick={handleClick}>
-        <div className={"bauchbinde_text"}>...</div>
+        <div className={"bauchbinde_text"}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="9" height="9" viewBox="0 0 24 24"><path fill="currentColor" d="M18 6.41L16.59 5L12 9.58L7.41 5L6 6.41l6 6z" /><path fill="currentColor" d="m18 13l-1.41-1.41L12 16.17l-4.59-4.58L6 13l6 6z" /></svg>
+        </div>
     </div>
 }
 
