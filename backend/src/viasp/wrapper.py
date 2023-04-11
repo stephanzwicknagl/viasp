4fea3f19590e69eddd0514931181fe4057139b55
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2023-04-11 10:30:28 -1000

    dropping show() to avoid error

--- backend/src/viasp/wrapper.py
+++ backend/src/viasp/wrapper.py
@@ -73,7 +73,8 @@ class ShowConnector:
         :param collect_variables: ``bool``
             default=True (collect variables from body as a tuple in the head literal)
         """
-        self.show()
+        self._database.set_target_stable_model(self._marked)
+        self._database._reconstruct()
         kwargs = {"head_name": head_name, "collect_variables": collect_variables}
         
         relaxed_prg = self._database.relax_constraints(**kwargs)
