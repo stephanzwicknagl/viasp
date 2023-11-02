a30b0b1aa249c590e2a3ee5b133824c20e259730
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2023-11-01 15:57:19 -1000

    add mockimport

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 # sys.path.append(os.path.join(os.path.abspath(os.pardir)))
-autodoc_mock_imports = ["clingo","clorm","networkx","graphviz","jsonschema"]
+autodoc_mock_imports = ["clingo", "_clingo", "graphviz"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
