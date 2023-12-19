5ce5d755c797404f3aa2ccc895f92fa9e2b452bd
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2023-12-18 20:29:20 -0700

    add test for potassco/viasp#40

--- backend/test/test_reification.py
+++ backend/test/test_reification.py
@@ -2,7 +2,7 @@ from typing import List
 
 import clingo
 import pytest
-from clingo.ast import ASTType, parse_string
+from clingo.ast import AST, ASTType, parse_string
 
 from viasp.asp.ast_types import (SUPPORTED_TYPES, UNSUPPORTED_TYPES,
                                  make_unknown_AST_enum_types)
@@ -18,7 +18,7 @@ def assertProgramEqual(actual, expected, message=None):
     assert actual == expected, message if message is not None else f"{expected} should be equal to {actual}"
 
 
-def parse_program_to_ast(prg: str) -> List[clingo.ast.AST]:
+def parse_program_to_ast(prg: str) -> List[AST]:
     parsed = []
     parse_string(prg, lambda rule: parsed.append(rule))
     return parsed
@@ -167,126 +167,3 @@ def test_disjunctions_in_head():
     h(1, q(X), (r(X),)) :- q(X), r(X). """
     assertProgramEqual(transform(rule), parse_program_to_ast(expected))
 
-
-def test_dependency_graph_creation():
-    program = "a. b :- a. c :- a."
-
-    analyzer = ProgramAnalyzer()
-    result = analyzer.sort_program(program)
-    assert len(result) == 2, "Facts should not be in the sorted program."
-    assert len(analyzer.dependants) == 2, "Facts should not be in the dependency graph."
-
-
-def test_negative_recursion_gets_grouped():
-    program = "a. b :- not c, a. c :- not b, a."
-
-    analyzer = ProgramAnalyzer()
-    result = analyzer.sort_program(program)
-    assert len(result) == 1, "Negative recursions should be grouped into one transformation."
-
-
-def multiple_non_recursive_rules_with_same_head_should_not_be_grouped():
-    program = "f(B) :- x(B). f(B) :- f(A), rel(A,B)."
-
-    analyzer = ProgramAnalyzer()
-    result = analyzer.sort_program(program)
-    assert len(result) == 2, "Multiple rules with same head that are not recursive should not be grouped."
-
-
-def test_sorting_facts_independent():
-    program = "c :- b. b :- a. a. "
-    transformer = ProgramAnalyzer()
-    result = transformer.sort_program(program)
-    assert len(result) == 2, "Facts should not be sorted."
-    assert str(next(iter(result[0].rules))) == "b :- a."
-    assert str(next(iter(result[1].rules))) == "c :- b."
-
-
-def test_sorting_behemoth():
-    program = "c(1). e(1). f(X,Y) :- b(X,Y). 1 #sum { X,Y : a(X,Y) : b(Y), c(X) ; X,Z : b(X,Z) : e(Z) } :- c(X). e(X) :- c(X)."
-    transformer = ProgramAnalyzer()
-    result = transformer.sort_program(program)
-    assert len(result) == 3
-    assert str(next(iter(result[0].rules))) == "e(X) :- c(X)."
-    assert str(next(iter(result[1].rules))) == "1 <= #sum { X,Y: a(X,Y): b(Y), c(X); X,Z: b(X,Z): e(Z) } :- c(X)."
-    assert str(next(iter(result[2].rules))) == "f(X,Y) :- b(X,Y)."
-
-
-def test_data_type_is_correct():
-    program = "d :- c. b :- a. a. c :- b."
-    transformer = ProgramAnalyzer()
-    result = transformer.sort_program(program)
-    assert len(result) > 0 and len(
-        result[0].rules) > 0, "Transformation should return something and the transformation should contain a rule."
-    a_rule = next(iter(result[0].rules))
-    data_type = type(a_rule)
-    assert data_type == clingo.ast.AST, f"{a_rule} should be an ASTType, not {data_type}"
-
-
-def get_reasons(prg, model):
-    ctl = clingo.Control()
-    ctl.add("base", [], prg)
-    ctl.add("base", [], "".join(model))
-    ctl.ground([("base", [])])
-    reasons = []
-    for x in ctl.symbolic_atoms.by_signature("h", 2):
-        reasons.append(x.symbol)
-    return set(reasons)
-
-
-def test_aggregate_in_body_of_constraint():
-    program = ":- 3 { assignedB(P,R) : paper(P) }, reviewer(R)."
-    transformer = ProgramAnalyzer()
-    result = transformer.sort_program(program)
-    assert len(result) == 1
-
-
-def test_minimized_causes_a_warning():
-    program = "#minimize { 1,P,R : assignedB(P,R), paper(P), reviewer(R) }."
-
-    transformer = ProgramAnalyzer()
-    transformer.sort_program(program)
-    assert len(transformer.get_filtered())
-
-
-def test_disjunction_causes_error_and_doesnt_get_passed():
-    program = "a; b."
-
-    transformer = ProgramAnalyzer()
-    program = transformer.sort_program(program)
-    assert len(transformer.get_filtered())
-    assert not len(program)
-
-
-def test_minimized_is_collected_as_pass_through():
-    program = "#minimize { 1,P,R : assignedB(P,R), paper(P), reviewer(R) }."
-
-    transformer = ProgramAnalyzer()
-    result = transformer.sort_program(program)
-    assert not len(result)
-    assert len(transformer.pass_through)
-
-
-def test_ast_types_do_not_intersect():
-    assert not SUPPORTED_TYPES.intersection(UNSUPPORTED_TYPES), "No type should be supported and unsupported"
-    known = SUPPORTED_TYPES.union(UNSUPPORTED_TYPES)
-    unknown = make_unknown_AST_enum_types()
-    assert not unknown.intersection(known), "No type should be known and unknown"
-
-
-@pytest.mark.skip(reason="Not implemented yet")
-def test_constraints_gets_put_last():
-    program = """
-    { assigned(P,R) : reviewer(R) } 3 :-  paper(P).
-     :- assigned(P,R), coi(R,P).
-     :- assigned(P,R), not classA(R,P), not classB(R,P).
-    assignedB(P,R) :-  classB(R,P), assigned(P,R).
-     :- 3 { assignedB(P,R) : paper(P) }, reviewer(R).
-    #minimize { 1,P,R : assignedB(P,R), paper(P), reviewer(R) }.
-    """
-    transformer = ProgramAnalyzer()
-    result = transformer.sort_program(program)
-    assert len(result) == 3
-    assert len(result[0].rules) == 1
-    assert len(result[1].rules) == 1
-    assert len(result[2].rules) == 3
