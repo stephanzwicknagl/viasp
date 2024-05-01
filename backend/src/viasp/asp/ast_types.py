2e8dbcccb6364d932bd14e13a79dbd5e97af3661
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2024-05-01 13:15:01 -1000

    Update ASTTypes to allow comments

--- backend/src/viasp/asp/ast_types.py
+++ backend/src/viasp/asp/ast_types.py
@@ -1,16 +1,23 @@
 from clingo.ast import ASTType
 
 SUPPORTED_TYPES = {
-    ASTType.Comparison, ASTType.Aggregate, ASTType.Rule, ASTType.Program, ASTType.ShowSignature, ASTType.Definition,
-    ASTType.Literal, ASTType.HeadAggregate, ASTType.HeadAggregateElement, ASTType.BodyAggregate, ASTType.Aggregate,
-    ASTType.ConditionalLiteral, ASTType.Guard, ASTType.Comparison, ASTType.SymbolicAtom, ASTType.Function,
-    ASTType.BodyAggregateElement, ASTType.BooleanConstant, ASTType.SymbolicAtom, ASTType.Variable, ASTType.SymbolicTerm,
-    ASTType.Interval, ASTType.UnaryOperation, ASTType.BinaryOperation, ASTType.Defined, ASTType.External, ASTType.ProjectAtom, ASTType.ProjectSignature, ASTType.ShowTerm, ASTType.Minimize, ASTType.Script
+    ASTType.Comparison, ASTType.Aggregate, ASTType.Rule, ASTType.Program,
+    ASTType.ShowSignature, ASTType.Definition, ASTType.Literal,
+    ASTType.HeadAggregate, ASTType.HeadAggregateElement, ASTType.BodyAggregate,
+    ASTType.Aggregate, ASTType.ConditionalLiteral, ASTType.Guard,
+    ASTType.Comparison, ASTType.SymbolicAtom, ASTType.Function,
+    ASTType.BodyAggregateElement, ASTType.BooleanConstant,
+    ASTType.SymbolicAtom, ASTType.Variable, ASTType.SymbolicTerm,
+    ASTType.Interval, ASTType.UnaryOperation, ASTType.BinaryOperation,
+    ASTType.Defined, ASTType.External, ASTType.ProjectAtom,
+    ASTType.ProjectSignature, ASTType.ShowTerm, ASTType.Minimize,
+    ASTType.Script, ASTType.Comment
 }
 
 UNSUPPORTED_TYPES = {
     ASTType.Disjunction,
 }
+
 
 def make_unknown_AST_enum_types():
     known = UNSUPPORTED_TYPES.union(SUPPORTED_TYPES)
